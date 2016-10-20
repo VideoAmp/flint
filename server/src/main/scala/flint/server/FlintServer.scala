4e9ef92186a6a34d3d4fed4635e4066c7cd68654
Michael Allman <michael@videoamp.com>
2016-10-19 18:47:26 -0700

    Modularize configuration of cluster service

--- server/src/main/scala/flint/server/FlintServer.scala
+++ server/src/main/scala/flint/server/FlintServer.scala
@@ -3,14 +3,16 @@ package server
 
 import messaging.akka.AkkaServer
 import service.aws.AwsClusterService
+import service.mock.MockClusterService
 
 import com.typesafe.config.{ Config, ConfigFactory, ConfigParseOptions }
+import com.typesafe.scalalogging.LazyLogging
 
 import configs.syntax._
 
 import rx._
 
-object FlintServer {
+object FlintServer extends LazyLogging {
   def main(args: Array[String]): Unit = {
     import Ctx.Owner.Unsafe._
 
@@ -19,20 +21,26 @@ object FlintServer {
     validateConfig(config)
 
     val flintConfig  = config.get[Config]("flint").value
-    val serverConfig = flintConfig.get[Config]("flint.server").value
+    val serverConfig = flintConfig.get[Config]("server").value
     val bindAddress  = serverConfig.get[String]("bind_address").value
     val serviceRoute = serverConfig.get[String]("service_route").value
 
     val bindInterface = bindAddress.takeWhile(_ != ':')
     val bindPort      = bindAddress.dropWhile(_ != ':').drop(1).toInt
 
-    val clusterService               = new AwsClusterService(flintConfig)
+    val clusterService =
+      serverConfig.get[String]("cluster_service").value match {
+        case "aws"  => new AwsClusterService(flintConfig)
+        case "mock" => new MockClusterService
+      }
+    logger.info("Using cluster service " + clusterService)
+
     val server: Server with Killable = AkkaServer(clusterService)
     val bindingFuture                = server.bindTo(bindInterface, bindPort, serviceRoute)
 
     bindingFuture.map { binding =>
+      logger.info(s"Websocket server online at http://$bindAddress$serviceRoute")
       // scalastyle:off println
-      println(s"Websocket server online at http://$bindAddress$serviceRoute")
       println("Press RETURN to stop...")
       // scalastyle:on println
       scala.io.StdIn.readLine
