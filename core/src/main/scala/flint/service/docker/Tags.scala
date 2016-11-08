74f392012e96a624330edc21a2b5ec9dec5bb9e0
Nick Lavers <nick.lavers@videoamp.com>
2016-11-08 11:09:41 -0800

    scalafmt

--- core/src/main/scala/flint/service/docker/Tags.scala
+++ core/src/main/scala/flint/service/docker/Tags.scala
@@ -2,7 +2,7 @@ package flint
 package service
 package docker
 
-import io.sphere.json.{fromJSON, JSON}
+import io.sphere.json.{ fromJSON, JSON }
 import io.sphere.json.generic.deriveJSON
 
 import org.apache.http.auth.UsernamePasswordCredentials
@@ -14,7 +14,7 @@ import org.apache.http.protocol.BasicHttpContext
 
 import better.files.InputStreamOps
 
-import scalaz.{ Failure, Success, ValidationNel}
+import scalaz.{ Failure, Success, ValidationNel }
 import scalaz.Validation.FlatMap.ValidationFlatMapRequested
 
 case class TagResponse(name: String, tags: List[String])
@@ -29,17 +29,17 @@ object TokenResponse {
 
 sealed trait Credentials
 case class UserPass(username: String, password: String) extends Credentials
-case class Token(key: String) extends Credentials
+case class Token(key: String)                           extends Credentials
 
 object Tags {
   val client = createDefault
 
   def apply(
-    repo: String,
-    auth: Option[Credentials] = None
+      repo: String,
+      auth: Option[Credentials] = None
   ): ValidationNel[String, List[DockerImage]] = {
     val tokenURL = "https://auth.docker.io/token?service=registry.docker.io&scope=repository:" +
-    repo + ":pull"
+        repo + ":pull"
 
     val tokenRequest = new HttpGet(tokenURL)
 
@@ -49,25 +49,20 @@ object Tags {
         tokenRequest.addHeader(new BasicHeader("Authorization", s"Basic ${key}"))
       case Some(UserPass(username, password)) =>
         tokenRequest.addHeader(
-          new BasicScheme()
-          .authenticate(
+          new BasicScheme().authenticate(
             new UsernamePasswordCredentials(username, password),
             tokenRequest,
             new BasicHttpContext()))
     }
 
-    val tokenJSON = client
-      .execute(tokenRequest)
-      .getEntity
-      .getContent
-      .lines
-      .mkString
+    val tokenJSON = client.execute(tokenRequest).getEntity.getContent.lines.mkString
 
     val authToken: ValidationNel[String, String] = fromJSON[TokenResponse](tokenJSON) match {
       case Success(TokenResponse(token)) => Success(token).toValidationNel
       case Failure(errors) =>
-        Failure(s"Failed to extract token from dockerhub server response JSON: ${tokenJSON}"
-          <:: errors.map(_.toString))
+        Failure(
+          s"Failed to extract token from dockerhub server response JSON: ${tokenJSON}"
+            <:: errors.map(_.toString))
     }
 
     authToken.flatMap { authToken =>
@@ -77,19 +72,15 @@ object Tags {
 
       tagRequest.setHeader("Authorization", s"Bearer ${authToken}")
 
-      val tagJSON = client
-        .execute(tagRequest)
-        .getEntity
-        .getContent
-        .lines
-        .mkString
+      val tagJSON = client.execute(tagRequest).getEntity.getContent.lines.mkString
 
       fromJSON[TagResponse](tagJSON) match {
         case Success(TagResponse(name, tags)) =>
           Success(tags.map(DockerImage(name, _))).toValidationNel
         case Failure(errors) =>
-          Failure(s"Failed to extract tags from dockerhub server response JSON: ${tagJSON}"
-            <:: errors.map(_.toString))
+          Failure(
+            s"Failed to extract tags from dockerhub server response JSON: ${tagJSON}"
+              <:: errors.map(_.toString))
       }
     }
   }
