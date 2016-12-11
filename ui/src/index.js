2b321ba9ba1edc7c2f96452834fa65fab1c26b2b
Edward Bramanti <edward@bramanti.org>
2016-12-10 17:01:27 -0800

    Move to using class for App

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reactStamp from 'react-stamp'
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
-import app from './App';
+import App from './App';
 import './index.css';
-
-const App = reactStamp(React).compose(app);
 
 ReactDOM.render(
   <App />,
