459c4597b8658135bdb82fab66696e17012ee8e3
Michael Allman <michael@videoamp.com>
2017-01-04 14:54:29 -0800

    Add a convenience apply method for ClusterSpec which generate new cluster ids

--- core/src/main/scala/flint/service/ClusterSpec.scala
+++ core/src/main/scala/flint/service/ClusterSpec.scala
@@ -13,3 +13,45 @@ case class ClusterSpec(
     workerInstanceType: String,
     numWorkers: Int,
     placementGroup: Option[String] = None)
+
+object ClusterSpec {
+  // Convenience constructors
+  def apply(
+      dockerImage: DockerImage,
+      owner: String,
+      ttl: Option[FiniteDuration],
+      idleTimeout: Option[FiniteDuration],
+      masterInstanceType: String,
+      workerInstanceType: String,
+      numWorkers: Int): ClusterSpec =
+    ClusterSpec(
+      ClusterId(),
+      dockerImage,
+      owner,
+      ttl,
+      idleTimeout,
+      masterInstanceType,
+      workerInstanceType,
+      numWorkers,
+      None)
+
+  def apply(
+      dockerImage: DockerImage,
+      owner: String,
+      ttl: Option[FiniteDuration],
+      idleTimeout: Option[FiniteDuration],
+      masterInstanceType: String,
+      workerInstanceType: String,
+      numWorkers: Int,
+      placementGroup: Option[String]): ClusterSpec =
+    ClusterSpec(
+      ClusterId(),
+      dockerImage,
+      owner,
+      ttl,
+      idleTimeout,
+      masterInstanceType,
+      workerInstanceType,
+      numWorkers,
+      placementGroup)
+}
