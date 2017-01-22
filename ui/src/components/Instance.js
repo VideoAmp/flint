331891e6b7104e291db552327eee16a64511d79b
Edward Bramanti <edward@bramanti.org>
2017-01-22 11:56:16 -0800

    Add new instance state colors, use indeterminate for unknown state
    - Use indeterminate circular progress for unknown state
    - Add isBeingTerminated state on instance to prevent multiple clicks on
    delete button

--- ui/src/components/Instance.js
+++ ui/src/components/Instance.js
@@ -1,30 +1,45 @@
 import React from 'react';
+import R from 'ramda';
 import {ListItem} from 'material-ui/List';
 import IconButton from 'material-ui/IconButton';
 import Avatar from 'material-ui/Avatar';
 import Trash from 'material-ui/svg-icons/action/delete';
 
-import { green300, yellow300, red300 } from 'material-ui/styles/colors';
+import { green300, yellow300, red300, cyan300 } from 'material-ui/styles/colors';
+import CircularProgress from 'material-ui/CircularProgress';
 
 import 'react-flexr/styles.css'
 
 const stateColorMap = {
+    "Pending": cyan300,
     "Starting": yellow300,
     "Running": green300,
+    // "Terminating":
     "Terminated": red300,
 };
 
+const leftAvatarStyles = { margin: 12.5 };
+
 export default class Instance extends React.Component {
+    state = {
+        isBeingTerminated: false,
+    }
+
     terminateWorker = (socket, { id }) => {
         const payload = JSON.stringify({ instanceId: id, "$type": "TerminateWorker" });
         socket.send(payload);
+        this.setState({ isBeingTerminated: true });
     }
 
     isTerminatable = (data, master) => {
-        return data.state !== "Terminated" && !master;
+        return data.state !== "Terminated" && !master && !this.state.isBeingTerminated;
     }
 
     getStatusIndicatorColor = (state) => stateColorMap[state];
+
+    getInstanceStateElement = (state) => R.has(state, stateColorMap) ?
+         <Avatar backgroundColor={stateColorMap[state]} size={15} style={leftAvatarStyles} /> :
+         <CircularProgress size={15} style={leftAvatarStyles}/>;
 
     render() {
         const {data, master, socket} = this.props;
@@ -39,7 +54,7 @@ export default class Instance extends React.Component {
         return (
             <ListItem
                 primaryText={`${data.ipAddress} ${master ? "Master" : "Worker"}`}
-                leftAvatar={ <Avatar backgroundColor={this.getStatusIndicatorColor(data.state)} size={15} style={{margin: 12.5}} />}
+                leftAvatar={this.getInstanceStateElement(data.state)}
                 rightIconButton={this.isTerminatable(data, master) ? rightIconButton : null}
                 disabled={true}
             />
