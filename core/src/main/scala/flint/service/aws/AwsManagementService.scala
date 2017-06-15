95c9051c82ddbfce2a786786d0c9e8e7a34dcb47
Michael Allman <michael@videoamp.com>
2017-06-15 15:14:26 -0700

    [CORE] Update AwsManagementService.sendCommand so that it accounts for all command invocation statuses before completing

--- core/src/main/scala/flint/service/aws/AwsManagementService.scala
+++ core/src/main/scala/flint/service/aws/AwsManagementService.scala
@@ -17,67 +17,70 @@ private[aws] class AwsManagementService(ssmClient: SsmClient)
       instances: Seq[Instance],
       comment: String,
       command: String,
-      executionTimeout: FiniteDuration): Future[Seq[Instance]] = {
-    val sendCommandRequest =
-      new SendCommandRequest()
-        .withDocumentName("AWS-RunShellScript")
-        .withInstanceIds(instances.map(_.id).asJava)
-        .withComment(comment.take(100))
-        .withParameters(
-          Map("commands" -> command, "executionTimeout" -> executionTimeout.toSeconds.toString)
-            .mapValues(value => Seq(value).asJava)
-            .asJava)
+      executionTimeout: FiniteDuration): Future[Seq[Instance]] =
+    if (instances.nonEmpty) {
+      val sendCommandRequest =
+        new SendCommandRequest()
+          .withDocumentName("AWS-RunShellScript")
+          .withInstanceIds(instances.map(_.id).asJava)
+          .withComment(comment.take(100))
+          .withParameters(
+            Map("commands" -> command, "executionTimeout" -> executionTimeout.toSeconds.toString)
+              .mapValues(value => Seq(value).asJava)
+              .asJava)
 
-    ssmClient.sendCommand(sendCommandRequest).map { command =>
-      val commandId = command.getCommandId
-      val listCommandInvocationsRequest =
-        new ListCommandInvocationsRequest().withCommandId(commandId)
+      ssmClient.sendCommand(sendCommandRequest).map(waitForCommandCompletion(_, instances))
+    } else {
+      Future.successful(Nil)
+    }
 
-      @annotation.tailrec
-      def waitForCommandCompletion(): Seq[Instance] = {
-        val instanceIds =
-          Await
-            .ready(
-              ssmClient.listCommandInvocations(listCommandInvocationsRequest),
-              Duration("5 min"))
-            .value
-            .get match {
-            case Success(invocations) =>
-              invocations.map { invocation =>
-                val instanceId = invocation.getInstanceId
-                CommandInvocationStatus.fromValue(invocation.getStatus) match {
-                  case CommandInvocationStatus.Success =>
-                    Right(Some(instanceId))
-                  case CommandInvocationStatus.Cancelled | CommandInvocationStatus.Cancelling |
-                      CommandInvocationStatus.Failed | CommandInvocationStatus.TimedOut =>
-                    logger.error(
-                      invocation.getComment + " for instance " + instanceId + " status: " +
-                        invocation.getStatus)
-                    Right(None)
-                  case _ => Left(Some(instanceId))
-                }
-              }
-            case Failure(ex) =>
-              throw new RuntimeException(
-                "Caught exception while listing invocations for " + command.getComment +
-                  " command id " + commandId,
-                ex)
+  @annotation.tailrec
+  private def waitForCommandCompletion(
+      command: Command,
+      instances: Seq[Instance]): Seq[Instance] = {
+    val commandId = command.getCommandId
+    val listCommandInvocationsRequest =
+      new ListCommandInvocationsRequest().withCommandId(commandId)
+    val commandInvocationInstanceIds =
+      Await
+        .ready(ssmClient.listCommandInvocations(listCommandInvocationsRequest), Duration("5 min"))
+        .value
+        .get match {
+        case Success(invocations) =>
+          invocations.map { invocation =>
+            val instanceId = invocation.getInstanceId
+            CommandInvocationStatus.fromValue(invocation.getStatus) match {
+              case CommandInvocationStatus.Success =>
+                Right(Some(instanceId))
+              case CommandInvocationStatus.Cancelled | CommandInvocationStatus.Cancelling |
+                  CommandInvocationStatus.Failed | CommandInvocationStatus.TimedOut =>
+                logger.error(
+                  invocation.getComment + " for instance " + instanceId + " status: " +
+                    invocation.getStatus)
+                Right(None)
+              case _ => Left(Some(instanceId))
+            }
           }
-
-        val pendingInstances = instanceIds.filter(_.isLeft).nonEmpty
-
-        if (pendingInstances) {
-          Thread.sleep(5000)
-          waitForCommandCompletion
-        } else {
-          val successfulInstanceIds =
-            instanceIds.flatMap(_.right.toOption).collect { case Some(id) => id }.toSet
-          instances.filter(instance => successfulInstanceIds.contains(instance.id))
-        }
+        case Failure(ex) =>
+          throw new RuntimeException(
+            "Caught exception while listing invocations for " + command.getComment +
+              " command id " + commandId,
+            ex)
       }
 
-      waitForCommandCompletion
+    val completedInvocationInstanceIds = commandInvocationInstanceIds.filter(_.isRight)
+    val otherInvocationInstanceIds     = commandInvocationInstanceIds.filter(_.isLeft)
+
+    if ((commandInvocationInstanceIds.size - otherInvocationInstanceIds.size) != instances.size) {
+      Thread.sleep(5000)
+      waitForCommandCompletion(command, instances)
+    } else {
+      val successfulInvocationInstanceIds =
+        completedInvocationInstanceIds
+          .flatMap(_.right.toOption)
+          .collect { case Some(id) => id }
+          .toSet
+      instances.filter(instance => successfulInvocationInstanceIds.contains(instance.id))
     }
   }
-
 }
