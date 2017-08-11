91ffc9736c8a121490be4f701c32765ac413a732
Michael Allman <michael@videoamp.com>
2017-08-11 14:12:14 -0700

    [CORE] Add InstanceState.scala, which should have been in the previous commit. Oops!

---
+++ core/src/main/scala/flint/InstanceState.scala
@@ -0,0 +1,25 @@
+package flint
+
+sealed trait InstanceState {
+  protected val name = toString
+}
+
+object InstanceState {
+  def apply(name: String): InstanceState = name match {
+    case Pending.name     => Pending
+    case Running.name     => Running
+    case Starting.name    => Starting
+    case Terminated.name  => Terminated
+    case Terminating.name => Terminating
+  }
+}
+
+case object Pending extends InstanceState
+
+case object Running extends InstanceState
+
+case object Starting extends InstanceState
+
+case object Terminated extends InstanceState
+
+case object Terminating extends InstanceState
